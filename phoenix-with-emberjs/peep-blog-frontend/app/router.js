e23196bb4184520f9c4191587510e8ac1585eb8b
julien michot <ju.michot@gmail.com>
2015-07-22 23:34:40 +0200

    Add edit page

--- phoenix-with-emberjs/peep-blog-frontend/app/router.js
+++ phoenix-with-emberjs/peep-blog-frontend/app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('posts', function() {
     this.route('new');
-    this.route('post', { path: '/:post_id' });
+    this.route('post', { path: '/:post_id' }, function() {
+      this.route('edit');
+    });
   });
 });
 
