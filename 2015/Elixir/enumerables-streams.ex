78cdb3920aa1ea9e60b0426ba85e60b1eb866340
julien michot <ju.michot@gmail.com>
2015-08-03 23:03:52 +0200

    Update enumerable knowledge

--- 2015/Elixir/enumerables-streams.ex
+++ 2015/Elixir/enumerables-streams.ex
@@ -15,6 +15,74 @@ Enum.map(%{1 => 2, 3 => 4}, fn {k, v} -> k * v end) # [2, 12]
 Enum.sum(Enum.filter(Enum.map(1..100_000, &(&1 * 3)), odd?))
 
 #########################################################################
+# ENUMERABLES elixirsips
+#########################################################################
+#### all?
+Enum.all?([1,2,3], fn(x) -> rem(x, 2) == 0 end) # false, because of 1 and 3
+Enum.all?([2,6], fn(x) -> rem(x, 2) == 0 end) # true
+Enum.all?([2,6], fn(x) -> x > 0 end) # true
+
+### any?
+Enum.any?([1,2,3], fn(x) -> rem(x, 2) == 0 end) # true (2)
+Enum.any?([1,5,3], fn(x) -> rem(x, 2) == 0 end) # false
+Enum.any?([1,5,3], fn(x) -> x < 0 end) # false
+
+### at, 0 based index
+Enum.at([1,2,3], 1) # 2
+Enum.at([1,2,3], 4) # nil, don't raise exception for index > size of the list
+
+### chunk/2 (you got not all element...)
+Enum.chunk([1,2,3,4], 2) # [[1, 2], [3, 4]]
+Enum.chunk([1,2,3,4], 4) # [[1, 2, 3, 4]]
+Enum.chunk([1,2,3,4], 42) # []
+### chunk/3
+Enum.chunk([1,2,3,4,5], 3, 2) # [[1, 2, 3], [3, 4, 5]]
+### chunk/4
+Enum.chunk([1,2,3,4,5], 3, 3, [1]) # [[1, 2, 3], [4, 5, 1]]
+Enum.chunk([1, 2, 3, 4, 5, 6], 4,4)  # [[1, 2, 3, 4]]
+Enum.chunk([1, 2, 3, 4, 5, 6], 4,4, [1]) # [[1, 2, 3, 4], [5, 6, 1]]
+Enum.chunk([1, 2, 3, 4, 5, 6], 4,4, [nil]) # [[1, 2, 3, 4], [5, 6, nil]]
+### chunk_by
+Enum.chunk_by([1, 2, 2, 3, 4, 4, 6, 7, 7], fn(x) -> rem(x, 2) == 1 end)
+
+### concat
+Enum.concat([[1],[2,3], [4]]) #[1, 2, 3, 4]
+Enum.concat(1..2, 3..3) #[1, 2, 3]
+
+## count/1
+Enum.count([2,3]) # 22
+## count/2
+Enum.count([:a, 1,2], fn(x) -> is_integer(x) end) # 2
+
+### drop
+Enum.drop([1,3,4,3,5,6], 3) #[3, 5, 6]
+### drop_while
+Enum.drop_while([1,3,4,3,5,6], fn(x)-> rem(x, 2) != 0 end) # [4, 3, 5, 6]
+### each
+Enum.each([1,2,3,4,5,6,7], fn(x) -> IO.puts("#{x}") end) # like each in ruby
+### empty?
+Enum.empty?([]) # true
+Enum.empty?([1]) # false
+### fetch
+Enum.fetch([1,2,34,5],2) # {:ok, 34}
+Enum.fetch([1,2,34,5],20) # :error
+Enum.fetch!([1,2,34,5],2) # 34
+Enum.fetch!([1,2,34,5],20) # ** (Enum.OutOfBoundsError) out of bounds error
+### filter
+Enum.filter([1,2,3,4,5,6], fn(x) -> rem(x,2) == 0 end) # [2, 4, 6]
+### filter_map
+Enum.filter_map([1,2,3,4,5,6], fn(x) -> rem(x,2) == 0 end, fn(x) -> "foo#{x}" end) # ["foo2", "foo4", "foo6"]
+
+### find
+Enum.find([-1,0,1], fn(x) -> x > 0 end) # 1
+Enum.find([-1,0,1], fn(x) -> x >= 0 end) # 0
+Enum.find([-1,0,1], fn(x) -> x > 2 end) # nil
+Enum.find([-1,0,1], :foo, fn(x) -> x > 2 end) # :foo : you can add default if not found, like fetch in ruby
+
+### first
+List.first([-1,0,1]) # -1, Enum.first deprecated, use Enum.at
+
+#########################################################################
 # STREAMS
 #########################################################################
 # NOTE use streams when big collection and don't want to generate
