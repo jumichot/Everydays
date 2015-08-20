57c7eb9697f0985ab3c8bcf3951b259bbc9b23c3
julien michot <ju.michot@gmail.com>
2015-08-20 19:42:04 +0200

    Continue zeldacat with genserver

---
+++ 2015/Elixir/Projects/zeldacat/lib/xy_component.ex
@@ -0,0 +1,23 @@
+defmodule XYComponent do
+  use GenEvent
+  # Public API
+  def get_position(entity) do
+    :gen_event.call(entity, XYComponent, :get_position)
+  end
+
+  def init(position) do
+    {:ok, position}
+  end
+
+  def handle_event({:move, {:y, new_y}}, {x, _}) do
+    {:ok, {x, new_y}}
+  end
+
+  def handle_event({:move, {:x, new_x}}, {_, y}) do
+    {:ok, {y, new_x}}
+  end
+
+  def handle_call(:get_position, position) do
+    {:ok, position, position}
+  end
+end
