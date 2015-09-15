48418a8e2b9a71043572c463149c711e524115df
julien michot <ju.michot@gmail.com>
2015-09-15 08:59:46 +0200

    Add route with dynamic param

--- 2015/Phoenix/phoenix_guide/web/controllers/hello_controller.ex
+++ 2015/Phoenix/phoenix_guide/web/controllers/hello_controller.ex
@@ -4,4 +4,10 @@ defmodule PhoenixGuide.HelloController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  # pattern matching against the params to set the messenger variable
+  def show(conn, %{"messenger" => messenger}) do
+    # pass the messenger variable to the template
+    render conn, "show.html", messenger: messenger
+  end
 end
