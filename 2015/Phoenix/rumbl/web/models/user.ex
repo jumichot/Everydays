1ea158ea4dc82bde16871e11e622ea8473714046
julien michot <ju.michot@gmail.com>
2015-10-14 17:25:27 +0200

    Add new changeset for users

--- 2015/Phoenix/rumbl/web/models/user.ex
+++ 2015/Phoenix/rumbl/web/models/user.ex
@@ -5,7 +5,7 @@ defmodule Rumbl.User do
     field :name, :string
     field :username, :string
     field :password, :string, virtual: true
-    field :password_hash, :string, virtual: true
+    field :password_hash, :string
 
     timestamps
   end
@@ -13,6 +13,23 @@ defmodule Rumbl.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
-    |> validate_length(:username, min: 1, max: 29)
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params) # that is the previous changeset
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
