b9bda4b5f76922ca4d4dea7a49771c8a45688ff5
julien michot <ju.michot@gmail.com>
2015-10-14 15:12:39 +0200

    Add error handling in users#new

--- 2015/Phoenix/rumbl/web/controllers/user_controller.ex
+++ 2015/Phoenix/rumbl/web/controllers/user_controller.ex
@@ -12,16 +12,20 @@ defmodule Rumbl.UserController do
     render conn, "show.html", user: user
   end
 
-  def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
-    conn
-    |> put_flash(:info, "#{user.name} user created !")
-    |> redirect(to: user_path(conn, :index))
-  end
-
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
