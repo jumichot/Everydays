470b4c89fd2895376e105741fd30b7870e6599b3
julien michot <ju.michot@gmail.com>
2015-10-15 10:33:14 +0200

    Protect with auth the /users

--- 2015/Phoenix/rumbl/web/router.ex
+++ 2015/Phoenix/rumbl/web/router.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Rumbl.Auth, repo: Rumbl.Repo
   end
 
   pipeline :api do
@@ -14,14 +15,9 @@ defmodule Rumbl.Router do
   end
 
   scope "/", Rumbl do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
     resources "/users", UserController
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
 end
