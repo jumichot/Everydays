e4cc7e863ad694608d3dd539159cca4e0f87df78
julien michot <ju.michot@gmail.com>
2016-02-07 12:17:28 +0100

    Add books model and index page

--- 2016/Hanami/bookshelf/spec/web/controllers/books/index_spec.rb
+++ 2016/Hanami/bookshelf/spec/web/controllers/books/index_spec.rb
@@ -4,8 +4,19 @@ RSpec.describe Web::Controllers::Books::Index do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
 
+  before do
+    BookRepository.clear
+
+    @book = BookRepository.create(Book.new(title: 'TDD', author: 'Kent Beck'))
+  end
+
   it 'is successful' do
     response = action.call(params)
     expect(response[0]).to eq 200
+  end
+
+  it 'exposes all books' do
+    action.call(params)
+    expect(action.exposures[:books]).to eq [@book]
   end
 end
--- 2016/Hanami/bookshelf/spec/web/features/list_books_spec.rb
+++ 2016/Hanami/bookshelf/spec/web/features/list_books_spec.rb
@@ -1,13 +1,19 @@
 require 'features_helper'
 
 describe 'List books' do
+  before do
+    BookRepository.clear
+    BookRepository.create(Book.new(title: 'PoEEA', author: 'Martin Fowler'))
+    BookRepository.create(Book.new(title: 'TDD', author: 'Kent Beck'))
+  end
+
   it 'display each books on the page' do
     visit '/books'
 
     expect(page).to have_content('All books')
 
     within '#books' do
-      expect(page.has_css?('.book', count: 5))
+      expect(page).to have_css('.book', count: 2)
     end
   end
 end
