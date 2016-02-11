def605e2938c2b13f7eaf0077e3bec121be31c1c
julien michot <ju.michot@gmail.com>
2016-02-11 11:06:57 +0100

    Add start-app package and make the counter work

--- 2016/Elm/elm-in-5-minutes.elm
+++ 2016/Elm/elm-in-5-minutes.elm
@@ -1,14 +1,29 @@
 import Html exposing (div, text, button)
+import StartApp.Simple as StartApp
+import Html.Events exposing (onClick)
 
 main =
-  view initialModel
+  StartApp.start
+    { model = initialModel
+    , view = view
+    , update = update
+    }
 
 initialModel = 0
 
-view model =
+type Action = Increment | Decrement
+
+update action model =
+  case action of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+
+view address model =
   div []
-    [ button [] [ text "+"]
-    , div [] [ text(toString model) ]
-    , button [] [text "-"]
-    ]
+  [ button [ onClick address Increment] [ text "+"]
+  , div [] [ text(toString model) ]
+  , button [ onClick address Decrement] [text "-"]
+  ]
 
