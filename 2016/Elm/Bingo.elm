c8463b98c626079797d0b33bde1fced99de2852d
julien michot <ju.michot@gmail.com>
2016-02-12 11:20:31 +0100

    Display entries with ul and li functions

--- 2016/Elm/Bingo.elm
+++ 2016/Elm/Bingo.elm
@@ -13,7 +13,44 @@ title message times =
     |> trimRight
     |> text
 
+
+pageHeader =
+  h1 [ ] [ title "Bingo !" 3 ]
+
+
+pageFooter =
+  footer [ ]
+    [ a [ href "http://julienmichot.fr" ]
+        [ text "Julien Michot website" ] ]
+
+newEntry phrase points id =
+  {
+    phrase = phrase,
+    points = points,
+    wasSpoken = False,
+    id = id
+  }
+entryItem phrase points =
+  li [ ]
+    [ span [ class "phrase" ] [ text phrase ],
+      span [ class "points" ] [ text (toString points) ]
+    ]
+
+entryList =
+  ul [ ]
+  [ entryItem "Future-Proof" 100,
+    entryItem "Doing Agile" 200 ]
+
+
+view =
+  div [ id "container" ]
+    [ pageHeader
+    , entryList
+    , pageFooter
+    ]
+
+
 main =
-  h1 [ id "logo", class "classy" ] [ title "Bingo !" 3 ]
+  view
 
 
