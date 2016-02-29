01209e0cc8aba3872a79e76883cb42dc6b7c6915
julien michot <ju.michot@gmail.com>
2016-02-29 19:32:10 -0400

    Add Artist structure

--- 2016/Elm/real_world_example/Main.elm
+++ 2016/Elm/real_world_example/Main.elm
@@ -3,6 +3,13 @@
 type alias Artist = { id: Int, name: String }
 type alias Model = List Artist
 
+artist : Json.Decode.Decoder Artist
+artist =
+  Json.Decode.object2 Artist
+    ("id" := Json.Decode.int)
+    ("name" := Json.Decode.string)
+
+
 init : Model
 init =
   []
@@ -27,6 +34,7 @@ actions =
 
 model : Signal Model
 model = Signal.foldp update init actions.signal
+
 
 main : Signal Html
 main =
