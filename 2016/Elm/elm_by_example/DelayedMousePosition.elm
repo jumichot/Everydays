688de8eafae1a560ec6aaa6928521ec8c78bb1ae
julien michot <ju.michot@gmail.com>
2016-03-12 21:37:20 +0100

    Refactore signals

--- 2016/Elm/elm_by_example/DelayedMousePosition.elm
+++ 2016/Elm/elm_by_example/DelayedMousePosition.elm
@@ -10,35 +10,40 @@ import Time exposing (delay)
 import Window
 
 
+
+customMouseSignal : Signal (Int, Int)
+customMouseSignal =
+  let
+    combineAndCenter (w, h) (x, y) =
+      (x-w//2,h//2-y)
+  in
+    Signal.map2 combineAndCenter Window.dimensions Mouse.position
+
+
+multiplyCustomMouseSignal : Int -> List (Signal (Int, Int))
+multiplyCustomMouseSignal repetition  =
+    List.repeat repetition customMouseSignal
+
+
+delayPositionWithDuration :  Int -> Signal (Int, Int) -> Signal ( Int, (Int, Int) )
+delayPositionWithDuration delayDuration mousePosition =
+  let
+    delayedPositionSignal =
+      delay (toFloat delayDuration*100) mousePosition
+  in
+    Signal.map (\mousePosition -> (delayDuration, mousePosition)) delayedPositionSignal
+
+
 combine : List (Signal a) -> Signal (List a)
 combine = foldr (Signal.map2 (::)) (constant [])
 
 
-centeredMousePosition : Signal (Int, Int)
-centeredMousePosition =
-  let
-    adjust (w, h) (x, y) = (x-w//2,h//2-y)
-  in
-    Signal.map2 adjust Window.dimensions Mouse.position
-
-
-repeatMousePositionSignal : Int -> List (Signal (Int, Int))
-repeatMousePositionSignal n  =
-    List.repeat n centeredMousePosition
-
-
 delayedMousePositions : List Int -> Signal (List (Int, (Int, Int)))
-delayedMousePositions rs =
+delayedMousePositions delaysList =
   let
-    positions = repeatMousePositionSignal (length rs)
-    delayedPositions =            -- List (Signal (Int, (Int, Int))
-      List.map2
-      (\r pos ->
-          let delayedPosition = delay (toFloat r*100) pos
-          in
-              (\pos -> (r,pos)) <~ delayedPosition)
-      rs
-      positions
+    positions = multiplyCustomMouseSignal (length delaysList)
+    delayedPositions =
+      List.map2 delayPositionWithDuration delaysList positions
   in
     combine delayedPositions
 
