a3a20b6871c5b352ca5be9e86e8227e073ff1a62
julien michot <ju.michot@gmail.com>
2016-03-19 15:02:09 +0100

    Multiple signals

--- 2016/Elm/elm-tutorial/ArchiAction.elm
+++ 2016/Elm/elm-tutorial/ArchiAction.elm
@@ -1,6 +1,7 @@
 module ArchiAction where
 
 import Mouse
+import Keyboard
 import Html exposing (..)
 
 type alias Model = { count : Int }
@@ -13,20 +14,27 @@ view model =
   text (toString model.count)
 
 
-type Action = NoOp | Increment
+type Action = NoOp | Increment | Decrement
 
 update : Action -> Model -> Model
 update action model =
   case action of
     Increment ->
       { model | count = model.count + 1 }
+    Decrement ->
+      { model | count = model.count - 1 }
     NoOp ->
       model
 
+keyPressesSignal =
+  Signal.map (\_ -> Decrement) Keyboard.presses
+
+mouseClickSignal =
+  Signal.map (\_ -> Increment) Mouse.clicks
 
 actionSignal : Signal.Signal Action
 actionSignal =
-  Signal.map (\_ -> Increment) Mouse.clicks
+  Signal.merge keyPressesSignal mouseClickSignal
 
 modelSignal : Signal Model
 modelSignal =
