4270d44234c56ce36cd7e848829fd3bc79563573
julien michot <ju.michot@gmail.com>
2016-03-19 15:40:46 +0100

    Finish elm architecture with manual mailboxes

--- 2016/Elm/elm-tutorial/ArchiAction.elm
+++ 2016/Elm/elm-tutorial/ArchiAction.elm
@@ -1,46 +1,88 @@
-module ArchiAction where
+module ArchiAction (..) where
 
 import Mouse
 import Keyboard
 import Html exposing (..)
+import Html.Events exposing (..)
 
-type alias Model = { count : Int }
+
+-- MODEL
+
+
+type alias Model =
+  { count : Int, maximum : Int }
+
 
 initialModel : Model
-initialModel = { count = 0 }
-
-view : Model -> Html
-view model =
-  text (toString model.count)
+initialModel =
+  { count = 0, maximum = 0 }
 
 
-type Action = NoOp | Increment | Decrement
+
+-- UPDATE
+
+
+type Action
+  = NoOp
+  | Increment
+  | Decrement
+  | Reset
+
 
 update : Action -> Model -> Model
 update action model =
   case action of
     Increment ->
-      { model | count = model.count + 1 }
+      let
+        currentValue =
+          model.count + 1
+      in
+        { model | count = currentValue , maximum = max currentValue model.maximum }
+
     Decrement ->
-      { model | count = model.count - 1 }
+      let
+        currentValue =
+          model.count - 1
+      in
+        { model | count = currentValue, maximum = max currentValue model.maximum }
+
+    Reset ->
+      initialModel
+
     NoOp ->
       model
 
-keyPressesSignal =
-  Signal.map (\_ -> Decrement) Keyboard.presses
 
-mouseClickSignal =
-  Signal.map (\_ -> Increment) Mouse.clicks
 
-actionSignal : Signal.Signal Action
-actionSignal =
-  Signal.merge keyPressesSignal mouseClickSignal
+-- Mailbox : renvoie record avec une adresse et un signal
+-- { address : Signal.Address Action , signal : Signal.Signal Action }
+
+
+mb : Signal.Mailbox Action
+mb =
+  Signal.mailbox NoOp
+
 
 modelSignal : Signal Model
 modelSignal =
-  Signal.foldp update initialModel actionSignal
+  Signal.foldp update initialModel mb.signal
+
+
+
+-- VIEW
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div
+    []
+    [ button [ onClick address Decrement ] [ text "Decrement" ]
+    , text (toString model)
+    , button [ onClick address Increment ] [ text "Increment" ]
+    , button [ onClick address Reset ] [ text "Reset" ]
+    ]
+
 
 main : Signal Html
 main =
-  Signal.map view modelSignal
-
+  Signal.map (view mb.address) modelSignal
