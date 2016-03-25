8e88d03e4e840ce53839b731cb0cd96a6b31a112
julien michot <ju.michot@gmail.com>
2016-03-25 12:19:39 +0100

    Add actions

--- 2016/Elm/elm-architecture/CounterList.elm
+++ 2016/Elm/elm-architecture/CounterList.elm
@@ -1,12 +1,24 @@
+module Main (..) where
+
 import StartApp.Simple as StartApp
-import Counter exposing(..)
-import Html exposing(..)
-import Html.Events exposing(..)
+import Counter exposing (..)
+import Html exposing (..)
+import Html.Events exposing (..)
+
 
 type alias Model =
   { counters : List Counter.Model, nextId : ID }
 
-type alias ID = Int
+
+type alias ID =
+  Int
+
+
+type Action
+  = Insert
+  | Remove
+  | Modify ID Counter.Action
+
 
 main =
   text "Hello world"
