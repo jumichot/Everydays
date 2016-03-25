df7ce32752f801ced44edc7bcc0d3156491208d5
julien michot <ju.michot@gmail.com>
2016-03-25 10:54:57 +0100

    Add counter pair with address forwarding in the view

--- 2016/Elm/elm-architecture/CounterPair.elm
+++ 2016/Elm/elm-architecture/CounterPair.elm
@@ -2,7 +2,39 @@ import StartApp.Simple as StartApp
 import Counter exposing(..)
 
 import Html exposing(..)
+import Html.Events exposing(..)
+
+type alias Model =
+  { topCounter : Counter.Model, bottomCounter : Counter.Model }
+
+init : Int -> Int -> Model
+init top bottom =
+  { topCounter = Counter.init top, bottomCounter = Counter.init bottom }
+
+type Action = Top Counter.Action | Bottom Counter.Action | Reset | NoOp
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Reset ->
+      init 0 0
+    Top act ->
+      { model | topCounter = Counter.update act model.topCounter }
+    Bottom act ->
+      { model | bottomCounter = Counter.update act model.bottomCounter }
+    NoOp ->
+      model
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div [] [
+    Counter.view (Signal.forwardTo address Top) model.topCounter
+    , Counter.view (Signal.forwardTo address Bottom) model.bottomCounter
+    , button [ onClick address Reset ] [ text "Reset" ]
+  ]
+
 
 main : Signal Html
 main =
-  StartApp.start { view = Counter.view, update = Counter.update, model = Counter.init(3) }
+  StartApp.start { view = view , update = update, model = init 0 0 }
