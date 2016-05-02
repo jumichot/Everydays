54ab82a803f61c93e936cff41c94bf00acfc4008
julien michot <ju.michot@gmail.com>
2016-05-02 12:38:46 +0200

    Add type alias and models for modeling application

--- 2016/Elm/dailydrip/todo-mvc/Main.elm
+++ 2016/Elm/dailydrip/todo-mvc/Main.elm
@@ -1,20 +1,44 @@
 import StartApp.Simple exposing(start)
 import Html exposing(..)
 
-type alias Model = Int
-type Action = NoOp
+
+type alias Todo =
+  { title : String
+  , completed : Bool
+  , editing : Bool
+  }
+
+type alias Todos = List Todo
+
+type FilterState = All | Active | Completed
+
+type alias Model =
+  { todos : Todos
+  , todo : Todo
+  , filter : FilterState
+  }
+
+type Action
+  = NoOp
+  | Add Todo
+  | Complete Todo
+  | Delete Todo
+  | Filter FilterState
 
 update : Action -> Model -> Model
 update action model =
   model
-
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div [] [text (toString model)]
 
 initialModel : Model
-initialModel = 0
+initialModel =
+  { filter = All
+  , todo = { title = "Test", completed = False, editing = False }
+  , todos = []
+  }
 
 main =
   start { view = view, model = initialModel, update = update }
