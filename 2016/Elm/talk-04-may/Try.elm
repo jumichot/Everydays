887103c4be13e4fa6a9cc181e5187240c12127aa
julien michot <ju.michot@gmail.com>
2016-05-04 16:55:30 +0200

    Step 5

--- 2016/Elm/talk-04-may/Try.elm
+++ 2016/Elm/talk-04-may/Try.elm
@@ -1,40 +1,55 @@
 import Html exposing (..)
+import Html.Attributes exposing (src)
 import Http
-import Json.Decode exposing (int, string, float, Decoder)
+import Json.Decode exposing (list, int, string, float, Decoder)
 import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 import Task exposing(Task, andThen)
+import Debug
 
 
 -- MODEL
 type alias Model =
-  { ip : String , about : String }
+   { id : Int , name : String, response : String , photo_link : String}
 
-initialModel = Model "0.0.0.0" "hello"
+initialModel = [Model -1 "hello" "YES" ""]
 
-mailbox : Signal.Mailbox Model
+mailbox : Signal.Mailbox (List Model)
 mailbox =
   Signal.mailbox initialModel
 
+viewModel : Model -> Html
+viewModel model = 
+  (div [] 
+  [ div [] [text model.name]
+  , div [] [text model.response]
+  , img [ src model.photo_link ] []
+  ])
+
 -- VIEW
-view : Model -> Html
-view model =
+view : List Model -> Html
+view models =
   div []
-  [ text (toString model.ip)
-  , br [] []
-  , text (toString model.about)
-  ]
+  (List.map viewModel models)
 
 -- HTTP
 fetchApi =
-  Http.get decoder "http://jsonip.com"
+  Http.get miam "http://localhost:3000/db"
 
 decoder : Decoder Model
 decoder =
   decode Model
-  |> required "ip" string
-  |> required "about" string
+  |> required "id" int
+  |> required "name" string
+  |> required "response" string
+  |> optional "photo_link" string "mnop"
+
+miam =
+  list decoder
 
 handleResponse data =
+  let
+      log = Debug.log "data" data
+  in
   Signal.send mailbox.address data
 
 port run : Task Http.Error ()
