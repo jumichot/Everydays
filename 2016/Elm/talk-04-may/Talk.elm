c3160283b38461d35f75987cb66913892f562a68
julien michot <ju.michot@gmail.com>
2016-05-04 17:19:21 +0200

    finish ?

--- 2016/Elm/talk-04-may/Talk.elm
+++ 2016/Elm/talk-04-may/Talk.elm
@@ -1,29 +1,28 @@
 import Html exposing (..)
 import Html.Attributes exposing (src)
 import Http
-import Json.Decode exposing (list, int, string, float, Decoder)
-import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 import Task exposing(Task, andThen)
-import Debug
+import Json.Decode exposing (object1, object2, (:=), list, int, string, float, Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 
-
--- MODEL
-type alias Model =
+-- Model
+type alias Model = 
    { id : Int , name : String, response : String , photo_link : String}
 
 initialModel = [Model -1 "hello" "YES" ""]
+
 
 mailbox : Signal.Mailbox (List Model)
 mailbox =
   Signal.mailbox initialModel
 
 viewModel : Model -> Html
-viewModel model = 
-  (div [] 
-  [ div [] [text model.name]
-  , div [] [text model.response]
+viewModel model =
+  div []
+  [text model.name
+  , text model.response
   , img [ src model.photo_link ] []
-  ])
+  ]
 
 -- VIEW
 view : List Model -> Html
@@ -31,7 +30,8 @@ view models =
   div []
   (List.map viewModel models)
 
--- HTTP
+
+-- API
 fetchApi : Task Http.Error (List Model)
 fetchApi =
   Http.get listDecoder "http://localhost:3000/db"
@@ -42,20 +42,17 @@ decoder =
   |> required "id" int
   |> required "name" string
   |> required "response" string
-  |> optional "photo_link" string "mnop"
+  |> optional "photo_link" string "nop"
 
-listDecoder =
+listDecoder=
   list decoder
 
-handleResponse data =
-  let
-      log = Debug.log "data" data
-  in
+callback data =
   Signal.send mailbox.address data
 
 port run : Task Http.Error ()
 port run =
-  fetchApi `andThen` handleResponse
+  fetchApi `andThen` callback
 
 main : Signal Html
 main =
