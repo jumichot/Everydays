4df888809f5f7efee29a0ae0157bc14867915942
julien michot <ju.michot@gmail.com>
2016-05-24 05:46:49 +0200

    Add user #index and #show

--- 2016/Phoenix/ProgrammingPhoenix/rumbl/lib/rumbl/repo.ex
+++ 2016/Phoenix/ProgrammingPhoenix/rumbl/lib/rumbl/repo.ex
@@ -6,7 +6,7 @@ defmodule Rumbl.Repo do
   """
 
   def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+    [%Rumbl.User{id: "1", name: "José Valim", username: "josevalim", password: "elixir"},
      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
      %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}] 
   end
