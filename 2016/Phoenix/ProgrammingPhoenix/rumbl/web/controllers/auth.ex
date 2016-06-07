123f3347398ddc5e0d12b7afd41cb0193bc00786
julien michot <ju.michot@gmail.com>
2016-06-07 10:52:05 +0200

    Add logout

--- 2016/Phoenix/ProgrammingPhoenix/rumbl/web/controllers/auth.ex
+++ 2016/Phoenix/ProgrammingPhoenix/rumbl/web/controllers/auth.ex
@@ -7,7 +7,7 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Rumbl.User, user_id)
+    user    = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
   end
 
@@ -15,7 +15,11 @@ defmodule Rumbl.Auth do
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
-    |> configure_session(renew: true) #renew the cookie and change the id
+    |> configure_session(renew: true) #renew the cookie and change the id, important if cookie stolen
+  end
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
   end
 
   import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
@@ -30,7 +34,7 @@ defmodule Rumbl.Auth do
       user ->
         {:error, :unauthorized, conn}
       true ->
-        dummy_checkpw() # for timing attacks
+        dummy_checkpw() #for timing attacks
         {:error, :not_found, conn}
     end
   end
