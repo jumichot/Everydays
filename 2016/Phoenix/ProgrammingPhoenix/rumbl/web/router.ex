ccf9c0fb787eab3d1f36e15a0a5d5e3e76629e2f
julien michot <ju.michot@gmail.com>
2016-06-07 11:11:56 +0200

    Generate Video ressource
    mix phoenix.gen.html Video videos user_id:references:users url:string title:string description:text

--- 2016/Phoenix/ProgrammingPhoenix/rumbl/web/router.ex
+++ 2016/Phoenix/ProgrammingPhoenix/rumbl/web/router.ex
@@ -19,6 +19,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, except: [:edit, :update, :delete]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
   end
 
   # Other scopes may use custom stacks.
