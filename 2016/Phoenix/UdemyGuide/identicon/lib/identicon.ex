4458f9ab5c2457dc1c5209bf04f822f3c24fdcf9
julien michot <ju.michot@gmail.com>
2016-11-26 15:54:25 +0700

    Add build_grid

--- 2016/Phoenix/UdemyGuide/identicon/lib/identicon.ex
+++ 2016/Phoenix/UdemyGuide/identicon/lib/identicon.ex
@@ -3,7 +3,24 @@ defmodule Identicon do
     input
     |> hash_string
     |> pick_color
+    |> build_grid
   end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid = hex_list
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
