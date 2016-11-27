2fbbc84a5c6619d6b50bb8e33458d0dfaed3c333
julien michot <ju.michot@gmail.com>
2016-11-27 18:28:59 +0700

    Add ueberauth dependency with configurations

--- 2016/Phoenix/UdemyGuide/discuss/config/config.exs
+++ 2016/Phoenix/UdemyGuide/discuss/config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: "d88082143f10f4721d3d",
+  client_secret: "f3bcc10a6d67171a04eb502d5cbcd1a3d8cc3855"
