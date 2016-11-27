def84db4e9c38064f6cf625e4a637010618e438b
julien michot <ju.michot@gmail.com>
2016-11-27 16:33:37 +0700

    Finish crud topics

--- 2016/Phoenix/UdemyGuide/discuss/web/controllers/topic_controller.ex
+++ 2016/Phoenix/UdemyGuide/discuss/web/controllers/topic_controller.ex
@@ -12,10 +12,11 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
 
+
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic created")
         |> redirect(to: topic_path(conn, :index))
@@ -23,4 +24,33 @@ defmodule Discuss.TopicController do
         render conn, "new.html", changeset: changeset
     end
   end
+
+  def edit(conn, %{"id" => topic_id} = params) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic_params} = params) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+         render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+  end
+
+  def delete(conn, %{"id" => topic_id} = params) do
+    topic = Repo.get!(Topic, topic_id) |> Repo.delete
+
+    conn
+    |> put_flash(:info, "Topic deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
 end
+
