90ab27e969fb4ad3b557d1587f63a08040b1c4ba
julien michot <ju.michot@gmail.com>
2017-05-21 15:58:05 +0100

    Add videos

--- 2017/Phoenix/programming-phoenix/rumbl/web/router.ex
+++ 2017/Phoenix/programming-phoenix/rumbl/web/router.ex
@@ -16,13 +16,15 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
-    get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-    #   pipe_through :api
-    # end
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
 end
