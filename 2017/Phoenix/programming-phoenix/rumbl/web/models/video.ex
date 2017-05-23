9a8e9aa690762da7243f14044d50079cd9556083
julien michot <ju.michot@gmail.com>
2017-05-23 14:40:31 +0100

    Add categories

--- 2017/Phoenix/programming-phoenix/rumbl/web/models/video.ex
+++ 2017/Phoenix/programming-phoenix/rumbl/web/models/video.ex
@@ -6,16 +6,16 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
 
-    timestamps()
+    timestamps
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
